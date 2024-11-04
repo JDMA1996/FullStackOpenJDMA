ba65153fa6e8ac2e269fe1df3a2279500cb432c1
José Mora <jdmoraalfaro@gmail.com>
2024-11-03 21:57:52 -0500

    se realiza ejercicio 1.1 componentes react

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -10,9 +10,9 @@ const Content =({parts,exercises})=>{
   return(
       <>
         
-        <p>{parts[0]} {exercises[0]}</p>   
-        <p>{parts[1]} {exercises[1]}</p>   
-        <p>{parts[2]} {exercises[2]}</p>         
+        <p>{parts[0]} {exercises[0]}</p>
+        <p>{parts[1]} {exercises[1]}</p>
+        <p>{parts[2]} {exercises[2]}</p>   
         
       </>
     )
